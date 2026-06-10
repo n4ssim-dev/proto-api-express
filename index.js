2386619181d064c8f9c8100139f49a5a6201ba75
Cédric Michaud <arcar13@gmail.com>
2026-06-10 15:23:06 +0200

    <Feat>Ajout requete tous les rdv

--- index.js
+++ index.js
@@ -145,7 +145,41 @@ app.get('/patients', (req, res) => {
     );
 });
 
+app.get('/allrdv', (req, res) => {
+   
+    
+    let db = new sqlite3.Database(path.resolve("proto.db"), (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log("Connection avec succès à la base de données SQLite.");
+    });
 
+    db.all(`SELECT * FROM rendez_vous `, [],
+        (err, rdv) => {
+            db.close((err) => {
+                if (err) {
+                    console.error(err.message);
+                }
+                console.log("Fermeture de la connexion.");
+            });
+
+            if (err) {
+                console.error(err.message);
+                return res.status(500).json({ "message": "Erreur serveur" });
+            }
+
+            
+                return res.status(200).json(
+                {
+                rdv:rdv
+                }
+            );
+            
+            
+        }
+    );
+});
 
 app.listen(3000, () => {
     console.log("Serveur démarré sur le port 3000");
