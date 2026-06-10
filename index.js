3bb8b3ae8f863f803a354ed410726c50154b140f
Nassim <achnassim34@gmail.com>
2026-06-10 15:21:47 +0200

    <Feat> Route médecin par id fonctionnel

--- index.js
+++ index.js
@@ -150,6 +150,47 @@ app.get("/rdv", (req, res) => {
 
 
 
+app.get("/medecin", (req, res) => {
+    let id = req.body.id;
+
+    let db = new sqlite3.Database(path.resolve("proto.db"), (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log("Connection avec succès à la base de données SQLite.");
+    });
+
+    db.get(`SELECT idMedecin, idService, specialite, nom_medecin, prenom_medecin FROM medecin WHERE idMedecin = ?`, [id],
+        (err, medecin) => {
+            db.close((err) => {
+                if (err) {
+                    console.error(err.message);
+                }
+                console.log("Fermeture de la connexion.");
+            });
+
+            if (err) {
+                console.error(err.message);
+                return res.status(500).json({ "message": "Erreur serveur" });
+            }
+
+            if (!medecin) {
+                return res.status(404).json(
+                    {"message": "Ce médecin n'existe pas."}
+                )
+            }
+
+            return res.status(200).json(
+                {
+                    "medecin": medecin
+                }
+            );
+        }
+    );
+})
+
+
+
 app.listen(3000, () => {
     console.log("Serveur démarré sur le port 3000");
 });
