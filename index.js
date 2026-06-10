d7067ecd0a59d65fb35b6248a37c0cea221f74e2
Cédric Michaud <arcar13@gmail.com>
2026-06-10 16:02:57 +0200

    <Feat>Ajout requete addPatient

--- index.js
+++ index.js
@@ -181,6 +181,47 @@ app.get('/allrdv', (req, res) => {
     );
 });
 
+
+app.post("/addpatient", (req, res) => {
+    let patient_mail = req.body.mail
+    let patient_nss = req.body.nss
+    let patient_nom = req.body.nom
+    let patient_prenom = req.body.prenom
+
+    let db = new sqlite3.Database(path.resolve("proto.db"), (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log("Connection avec succès à la base de données SQLite.");
+    });
+
+    db.get(`INSERT INTO patient (idPatient, nom, prenom, NSS, mail) VALUES ((SELECT IFNULL(MAX(idPatient), 0) + 1 FROM patient), ?, ?, ?, ?)`, [patient_nom, patient_prenom, patient_nss, patient_mail],
+        (err, patient) => {
+            db.close((err) => {
+                if (err) {
+                    console.error(err.message);
+                }
+                console.log("Fermeture de la connexion.");
+            });
+
+            if (err) {
+                console.error(err.message);
+                return res.status(500).json({ "message": "Erreur serveur" });
+            }
+
+            
+            return res.status(200).json(
+                {
+                "message": "ajout patient réussi",
+                
+                }
+            );
+        }
+    );
+})
+
+
+
 app.listen(3000, () => {
     console.log("Serveur démarré sur le port 3000");
 });
