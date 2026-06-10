e34f0092f70bf44169a0ac340f177b4d1b7d825c
Nassim <achnassim34@gmail.com>
2026-06-10 15:23:20 +0200

    <Feat> route médecin par service fonctionnel

--- index.js
+++ index.js
@@ -191,6 +191,41 @@ app.get("/medecin", (req, res) => {
 
 
 
+app.get("/medecin/service", (req, res) => {
+    let idService = req.body.idService;
+
+    let db = new sqlite3.Database(path.resolve("proto.db"), (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log("Connection avec succès à la base de données SQLite.");
+    });
+
+    db.all(`SELECT idMedecin, idService, specialite, nom_medecin, prenom_medecin FROM medecin WHERE idService = ?`, [idService],
+        (err, medecins) => {
+            db.close((err) => {
+                if (err) {
+                    console.error(err.message);
+                }
+                console.log("Fermeture de la connexion.");
+            });
+
+            if (err) {
+                console.error(err.message);
+                return res.status(500).json({ "message": "Erreur serveur" });
+            }
+
+            return res.status(200).json(
+                {
+                    "medecins": medecins
+                }
+            );
+        }
+    );
+})
+
+
+
 app.listen(3000, () => {
     console.log("Serveur démarré sur le port 3000");
 });
