3bda125415fc7034d6498bc5920f274f69479cd6
Cédric Michaud <arcar13@gmail.com>
2026-06-10 14:48:09 +0200

    <Feat>Ajout requete patient/id

--- index.js
+++ index.js
@@ -67,6 +67,46 @@ app.post("/login", (req, res) => {
     );
 })
 
+app.get('/patientId', (req, res) => {
+    let patient_id = req.body.id
+    
+    let db = new sqlite3.Database(path.resolve("proto.db"), (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log("Connection avec succès à la base de données SQLite.");
+    });
+
+    db.get(`SELECT * FROM patient WHERE patient.idPatient = ?`, [patient_id],
+        (err, patient) => {
+            db.close((err) => {
+                if (err) {
+                    console.error(err.message);
+                }
+                console.log("Fermeture de la connexion.");
+            });
+
+            if (err) {
+                console.error(err.message);
+                return res.status(500).json({ "message": "Erreur serveur" });
+            }
+
+            
+            return res.status(200).json(
+                {
+                "Patient": {
+                    "id": patient.idPatient,
+                    "nom": patient.nom,
+                    "prenom": patient.prenom,
+                    "mail": patient.mail,
+                    "N° SS": patient.NSS
+                }
+                }
+            );
+        }
+    );
+});
+
 
 
 app.listen(3000, () => {
