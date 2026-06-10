82fb9ee808431dbc9c2ea39a59ed91e74aa1df70
Nassim <achnassim34@gmail.com>
2026-06-10 12:19:22 +0200

    <Feat> Connecteur sqlite3 avec requete initial fonctionnel

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const path = require("path");
+const sqlite3 = require("sqlite3");
 const app = express();
 
 app.use(express.json());
@@ -17,6 +19,36 @@ app.get("/health", (req,res) => {
     )
 })
 
+// Source - https://stackoverflow.com/a/53783495
+// Posted by Paul, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-06-10, License - CC BY-SA 4.0
+
+app.post("/", (req, res) => {
+    let db = new sqlite3.Database(path.resolve("proto.db"), (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log("Connection avec succès à la base de données SQLite.");
+    });
+
+    db.get(`SELECT * FROM users WHERE idUser = ?`, [req.body.id], (err, row) => {
+    db.close((err) => {
+        if (err) {
+        console.error(err.message);
+        }
+        console.log("Fermeture de la connexion.");
+    });
+
+    if (err) {
+        console.error(err.message);
+        return res.status(500).json({ "message": "Erreur serveur" });
+    }
+
+    res.status(200).json(row);
+    });
+});
+
+
 app.post("/login", (req,res) => {
     let user_mail = req.body.mail
     let user_password = req.body.pass
@@ -33,7 +65,7 @@ app.post("/login", (req,res) => {
         )
     } 
     
-    else if (user_password == user.pass && user_email == user.email) {
+    else {
         return res.status(200).json(
             {
             "message": "Connexion réussie",
